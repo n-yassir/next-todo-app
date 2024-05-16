39894e2eb018b24b2e957ffb37996dfd9f170627
n-yassir <101258459+n-yassir@users.noreply.github.com>
2024-05-16 17:56:48 +0200

    deploy: Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,18 +5,18 @@ export default function Footer() {
   return (
     <footer className="container border-t border-slate-800 py-8 flex items-center justify-between">
       <Link
-        href="https://raymondkneipp.com"
+        href="https://ayaline.com"
         className="flex items-center gap-8 opacity-60 hover:opacity-100 transition-opacity"
         target="_blank"
       >
         <Image
-          src="https://raymondkneipp.com/logo.svg"
-          alt="Raymond Kneipp Logo"
+          src="https://www.ayaline.com/wp-content/uploads/2022/09/cropped-favicon-32x32-1-192x192.png"
+          alt="Yassir Nasser Logo"
           width={50}
           height={50}
         />
         <span>
-          Created by Raymond Kneipp
+          Created by Yassir Nasser
         </span>
       </Link>
     </footer>
